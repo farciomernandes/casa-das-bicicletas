30fe176a5f7891cf99793210440b6d924cc0905a
Diego Vieira <dvsdiegovieira@gmail.com>
2024-05-10 10:09:20 -0300

    feat: adding query params in dblistcity

--- src/core/application/city/db-list-city.ts
+++ src/core/application/city/db-list-city.ts
@@ -2,14 +2,15 @@ import { IDbListCityRepository } from '@/core/domain/protocols/db/city/list-city
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CityRepository } from '@/core/domain/protocols/repositories/city';
 import { CityListDto } from '@/presentation/dtos/city/city-model.dto';
+import { CityParamsDto } from '@/presentation/dtos/city/params-city.dto';
 
 @Injectable()
 export class DbListCity implements IDbListCityRepository {
   constructor(private readonly cityRepository: CityRepository) {}
 
-  async getAll(): Promise<CityListDto> {
+  async getAll(querryParams: CityParamsDto): Promise<CityListDto> {
     try {
-      return await this.cityRepository.getAll();
+      return await this.cityRepository.getAll(querryParams);
     } catch (error) {
       throw new InternalServerErrorException(error.message);
     }
