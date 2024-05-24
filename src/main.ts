7f111ea7e320b606fc1d9e05a53f1dd664fb57ba
Marcio Fernandes <farciomernandes@gmail.com>
2024-05-24 10:42:29 -0300

    chore: remove swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as basicAuth from 'express-basic-auth';
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AllExceptionsFilter } from './shared/filter/http-exception.filter';
 import { LogServerStatus } from './shared/helpers/log-server-status';
@@ -16,38 +14,8 @@ async function bootstrap() {
     app.useGlobalFilters(new AllExceptionsFilter());
     LogServerStatus.logEnv({ currentEnv });
 
-    const setupSwagger = (application: INestApplication) => {
-      let swaggerRoute = '/';
-      if (currentEnv !== 'production') {
-        application.use(
-          ['/docs', '/docs-json'],
-          basicAuth({
-            challenge: true,
-            users: {
-              [configService.get('CASA_DAS_BICICLETAS_SWAGGER_USERNAME')]:
-                configService.get('CASA_DAS_BICICLETAS_SWAGGER_PASSWORD'),
-            },
-          }),
-        );
-        swaggerRoute = '/docs';
-      }
-
-      const config = new DocumentBuilder()
-        .setTitle(configService.get('CASA_DAS_BICICLETAS_SWAGGER_TITLE'))
-        .setDescription(
-          configService.get('CASA_DAS_BICICLETAS_SWAGGER_DESCRIPTION'),
-        )
-        .setVersion(configService.get('CASA_DAS_BICICLETAS_SWAGGER_VERSION'))
-        .addBearerAuth()
-        .build();
-
-      const document = SwaggerModule.createDocument(application, config);
-      SwaggerModule.setup(swaggerRoute, application, document);
-    };
     app.enableCors();
     app.useGlobalPipes(new ValidationPipe());
-    setupSwagger(app);
-
     const port = configService.get('PORT') || 3000;
     await app.listen(port);
     LogServerStatus.logSuccess({ isProduction: false, port, host });
